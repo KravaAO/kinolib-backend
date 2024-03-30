e20d420a2a1c95dcd2529969592ced34e72c544c
Artem <84147757+KravaAO@users.noreply.github.com>
2024-03-31 01:10:16 +0200

    first serial parser

---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function parseHTML() {
+    try {
+        const response = await axios.get('https://uaserials.pro/');
+        const $ = cheerio.load(response.data);
+
+        $('.short-item').each((index, element) => {
+            const title = $(element).find('.th-title').text().trim();
+            const originalTitle = $(element).find('.th-title-oname').text().trim();
+            const seasonInfo = $(element).find('.short-label-level-1').text().trim();
+            const episodeInfo = $(element).find('.short-label-level-2').text().trim();
+            const imageUrl = $(element).find('img').attr('src');
+            const link = $(element).find('a').attr('href');
+
+            console.log('Title:', title);
+            console.log('Original Title:', originalTitle);
+            console.log('Season Info:', seasonInfo);
+            console.log('Episode Info:', episodeInfo);
+            console.log('Image URL:', imageUrl);
+            console.log('Link:', link);
+            console.log('------------------------------------');
+        });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+parseHTML();
